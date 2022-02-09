cd3f4fcd5d2a5112962134224a97a7d9dbf475e5
duquiang <angela.duquino@adidas.com>
2022-02-09 08:37:08 -0500

    Run test without Browserstack

--- playwright.config.ts
+++ playwright.config.ts
@@ -103,15 +103,15 @@ const config: PlaywrightTestConfig = {
 
     // -- BrowserStack Projects --
     // name should be of the format browser@browser_version:os os_version@browserstack
-    {
-      name: 'chrome@desktop@browserstack',
-    },
-    {
-      name: 'edge@desktop@browserstack',
-    },
-    {
-      name: 'firefox@desktop@browserstack',
-    },
+    // {
+    //   name: 'chrome@desktop@browserstack',
+    // },
+    // {
+    //   name: 'edge@desktop@browserstack',
+    // },
+    // {
+    //   name: 'firefox@desktop@browserstack',
+    // },
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
