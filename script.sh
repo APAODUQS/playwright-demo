f1ddaaa7629c1e01345ebd42622e641d9998e5d8
duquiang <angela.duquino@adidas.com>
2022-07-25 11:32:33 -0500

    Fix script

--- script.sh
+++ script.sh
@@ -1,5 +1,34 @@
-export STATUS="$1"
-export DESCRIPTION="$2"
+export JIRAUSERNAME=$1
+export JIRAPASS=$2
+export SUMMARY=$3
+export TICKET=$4
+export JIRA_URL="https://paoduq.atlassian.net"
 
-echo "Changing the status for the test cases to ${STATUS}"
-echo "The description is ${DESCRIPTION}"
+echo 'Creating the Test Execution'
+export TEST_EXECUTION=$(curl --request POST -u ${JIRAUSERNAME}:${JIRAPASS} ${JIRA_URL}/rest/api/2/issue \
+--header 'Content-Type: application/json' \
+--data-raw "{
+   \"fields\": {
+       \"project\":
+       {
+          \"key\": \"DESK\"
+       },
+       \"summary\": \"Automation Testing | Execute automated tests for ${SUMMARY}\",
+       \"description\": \"Test Execution for running the automated tests\",
+       \"issuetype\": {
+          \"name\": \"Test Execution\"
+       }
+   }
+}" | cut -d '"' -f 8)
+
+echo 'Adding tests to Test Execution'
+curl -X POST -H "Content-Type: application/json" -u ${JIRAUSERNAME}:${JIRAPASS} ${JIRA_URL}/rest/raven/1.0/api/testexec/${TEST_EXECUTION}/test \
+--data-raw "{
+   \"add\": [
+        \"${TICKET}\"
+    ]
+}"
+
+echo $TEST_EXECUTION
+
+
